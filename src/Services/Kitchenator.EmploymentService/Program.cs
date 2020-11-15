c28a898193e9da4e489c856faaa6c40c43a581f3
Pedro Dias <pedro@dolittle.com>
2020-11-15 20:14:00 +0100

    Add ASP.Net Core Logging

--- src/Services/Kitchenator.EmploymentService/Program.cs
+++ src/Services/Kitchenator.EmploymentService/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 
 namespace Kitchenator.EmploymentService
@@ -16,9 +17,13 @@ namespace Kitchenator.EmploymentService
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            .ConfigureLogging(logging => {
+                logging.ClearProviders();
+                logging.AddConsole();
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
     }
 }
