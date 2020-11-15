88018817811436cf81466feb2195942c14947e5a
Pedro Dias <pedro@dolittle.com>
2020-11-15 20:14:46 +0100

    Add logging to AspNetContainer

--- src/Dolittle.AspNET.ConfigurationExtensions/DolittleAspNetContainer.cs
+++ src/Dolittle.AspNET.ConfigurationExtensions/DolittleAspNetContainer.cs
@@ -1,6 +1,7 @@
 ﻿using Dolittle.SDK.DependencyInversion;
 using Dolittle.SDK.Execution;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
 
 namespace Dolittle.AspNET.ConfigurationExtensions
@@ -8,18 +9,22 @@ namespace Dolittle.AspNET.ConfigurationExtensions
     public class DolittleAspNetContainer : IContainer
     {
         readonly IServiceProvider _serviceProvider;
+        readonly ILogger<DolittleAspNetContainer> _logger;
 
-        public DolittleAspNetContainer(IServiceProvider serviceProvider)
+        public DolittleAspNetContainer(IServiceProvider serviceProvider, ILogger<DolittleAspNetContainer> logger)
         {
             _serviceProvider = serviceProvider;
+            _logger = logger;
         }
         public object Get(Type service, ExecutionContext context)
         {
+            _logger.LogInformation($"Returning service {service.Name}");
             return _serviceProvider.GetService(service);
         }
 
         public T Get<T>(ExecutionContext context) where T : class
         {
+            _logger.LogInformation($"Returning T = '{typeof(T)}'");
             return _serviceProvider.GetService<T>();
         }
     }
