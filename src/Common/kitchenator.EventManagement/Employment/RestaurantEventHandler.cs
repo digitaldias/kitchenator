15a493c6a40544e9486577fa7253640dbb6bdead
Pedro Dias <pedro@dolittle.com>
2020-11-15 17:00:52 +0100

    Move construction of Restaurant in EventHandler to its base class

--- src/Common/kitchenator.EventManagement/Employment/RestaurantEventHandler.cs
+++ src/Common/kitchenator.EventManagement/Employment/RestaurantEventHandler.cs
@@ -2,13 +2,13 @@
 using Dolittle.SDK.Events.Handling;
 using Dolittle.SDK.Events.Store;
 using kitchenator.Domain;
-using kitchenator.Domain.BoundedContexts;
 using kitchenator.Domain.Concepts.Employees;
 using kitchenator.Domain.Contracts;
 using System;
 using System.Threading.Tasks;
 using Realestate = kitchenator.Domain.Events.Realestate;
 using RealEstate = kitchenator.Domain.Concepts.Realestate;
+using Employees  = kitchenator.Domain.Concepts.Employees;
 
 namespace kitchenator.EventManagement.Employment
 {
@@ -28,15 +28,7 @@ namespace kitchenator.EventManagement.Employment
         {            
             if (evt is RealEstate.Restaurant restaurant)
             {
-                var newRestaurant = new Restaurant
-                {
-                    Id                = restaurant.Id,
-                    Name              = restaurant.Name,
-                    Address           = restaurant.Address,
-                    EmployeeCapacity  = restaurant.ChefCapacity,
-                    CloseDate         = DateTime.MaxValue,
-                    CurrentlyEmployed = 0
-                };
+                var newRestaurant = new Employees.Restaurant(restaurant);
                 await _restaurantRepo.Upsert(newRestaurant);
             }
         }
