a0fd6f9c34893599a3e1d330d1242b8e58c79f81
Pedro Dias <pedro@dolittle.com>
2020-11-16 07:49:50 +0100

    Add Google map to experiment with JSInterop

---
+++ src/Applications/ASPNet/Kitchenator.Web.PropertyManager/wwwroot/scripts/script.js
@@ -0,0 +1,9 @@
+﻿function initializeMap() {
+    // 60.2362325,17.7927358,6.19z
+    var latlng = new google.maps.LatLng(60.2362325, 17.7927358);
+    var options = {
+        zoom: 6.19, center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map"), options);
+}
